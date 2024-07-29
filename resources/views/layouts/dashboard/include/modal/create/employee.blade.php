9465300bf73c419fe848da48dbbfb7d2ae472517
Erkek <baxaisov.erke@yandex.ru>
2024-07-29 15:39:31 +0400

    fixed access layout

--- resources/views/layouts/dashboard/include/modal/create/employee.blade.php
+++ resources/views/layouts/dashboard/include/modal/create/employee.blade.php
@@ -1,4 +1,4 @@
-<div class="create-modal" id="create_employee" style="display: none">
+<div class="modal fade" id="create_employee" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content" style="background-color: #fff;">
             <div class="modal-header">
@@ -7,43 +7,43 @@
             <div class="modal-body p-4">
                 <form method="post" id="create_employee_form" class="d-flex flex-column gap-2"
                       onsubmit="createEmployee();return false;">
-                    <div class="form-group">
+                    <div class="d-flex justify-content-center">
                         <label class="col-sm-4">ФИО</label>
                         <div class="col-sm-8">
                             <input type="text" class="form-control" name="name" required="">
                         </div>
                     </div>
-                    <div class="form-group">
+                    <div class="d-flex justify-content-center">
                         <label class="col-sm-4">Email</label>
                         <div class="col-sm-8">
                             <input type="email" class="form-control" name="email" required="">
                         </div>
                     </div>
-                    <div class="form-group">
+                    <div class="d-flex justify-content-center">
                         <label class="col-sm-4">Логин</label>
                         <div class="col-sm-8">
                             <input type="text" class="form-control" name="login" required="">
                         </div>
                     </div>
-                    <div class="form-group">
+                    <div class="d-flex justify-content-center">
                         <label class="col-sm-4">Пароль</label>
                         <div class="col-sm-8">
                             <input type="password" class="form-control" name="password" required="">
                         </div>
                     </div>
-                    <div class="form-group">
+                    <div class="d-flex justify-content-center">
                         <label class="col-sm-4">Номер телефона</label>
                         <div class="col-sm-8">
                             <input type="text" class="form-control" name="phone">
                         </div>
                     </div>
-                    <div class="form-group">
+                    <div class="d-flex justify-content-center">
                         <label class="col-sm-4">Дата рождения</label>
                         <div class="col-sm-8">
                             <input type="date" class="form-control" name="date_of_birth">
                         </div>
                     </div>
-                    <div class="form-group">
+                    <div class="d-flex justify-content-center">
                         <label class="col-sm-4">Пол</label>
                         <div class="col-sm-8">
                             <select name="gender" class="form-control">
@@ -52,7 +52,7 @@
                             </select>
                         </div>
                     </div>
-                    <div class="form-group">
+                    <div class="d-flex justify-content-center">
                         <label class="col-sm-4">Статус</label>
                         <div class="col-sm-8">
                             <select name="is_active" class="form-control">
@@ -61,15 +61,15 @@
                             </select>
                         </div>
                     </div>
-                    <h3 class="bc-post-title">Определение уровня доступа</h3>
-                    <div class="form-group">
+                    <h4 class="bc-post-title m-0 pt-4">Определение уровня доступа</h4>
+                    <div class="d-flex justify-content-center pt-3">
                         <label class="col-sm-4">Год выпуска</label>
                         <div class="col-sm-8">
                             <select class="form-control" name="year_id" id="years_list" required="">
                             </select>
                         </div>
                     </div>
-                    <div class="form-group">
+                    <div class="d-flex justify-content-center">
                         <label class="col-sm-4">Факультет</label>
                         <div class="col-sm-8">
                             <select class="form-control" name="faculty_id" id="faculties_list">
@@ -77,7 +77,7 @@
                             </select>
                         </div>
                     </div>
-                    <div class="form-group">
+                    <div class="d-flex justify-content-center">
                         <label class="col-sm-4">Кафедры</label>
                         <div class="col-sm-8">
                             <select name="departments_ids[]" id="departments_menu_list"
@@ -88,11 +88,12 @@
                             </select>
                         </div>
                     </div>
-                    <div class="modal-footer br-none">
-                        <button type="button" class="btn btn-danger" data-dismiss="modal"
-                                onclick="closeModal('create_employee')">Закрыть
+                    <div class="modal-footer br-none pe-0">
+                        <button type="submit" class="btn btn-secondary fs-14 text-grey py-1" data-bs-dismiss="modal">
+                            Применить
                         </button>
-                        <button type="submit" class="btn btn-success" onclick="closeModal('create_employee')">Создать
+                        <button type="button" class="btn btn-grey border-radius-5 fs-14 text-grey py-1" data-bs-dismiss="modal">
+                            Закрыть
                         </button>
                     </div>
                 </form>
