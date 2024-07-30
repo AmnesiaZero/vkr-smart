3fcc1e777862b065286467ffd566ba8c5cf1073b
Erkek <baxaisov.erke@yandex.ru>
2024-07-30 17:22:15 +0400

    changing the Personal Account and Login buttons

--- resources/views/layouts/site/main.blade.php
+++ resources/views/layouts/site/main.blade.php
@@ -65,12 +65,38 @@
                     </li>
                     <li><a class="nav-link" href="https://api.vkr-vuz.ru" target="_blank">API</a></li>
                     <li>
-                        <a href="/login" class="nav-link">
-		                        		<span class="badge br-40 br-green-1 d-flex align-items-center"
-                                              style="padding-top: 7px; padding-bottom: 7px;">
-		                        			<span class="fs-16 text-black ps-1 pe-1">Вход</span>
-		                        		</span>
-                        </a>
+
+                        @if(\Illuminate\Support\Facades\Auth::check())
+                            <div class="d-flex gap-3 position-relative">
+                                <a href="/dashboard" class="nav-link">
+                                    <span class="badge br-40 br-green-1 d-flex align-items-center"
+                                          style="padding-top: 7px; padding-bottom: 7px;">
+                                        <span class="fs-16 text-black ps-1 pe-1">Личный кабинет</span>
+                                    </span>
+                                </a>
+
+                                <a href="#" class="btn dropdown-toggle pt-0" type="button" id="dropdownMenu" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenu">
+                                    <li><a class="dropdown-item" href="/organization-settings">Настройки</a></li>
+                                    <li><a class="dropdown-item" href="/organization-works">Работы</a></li>
+                                    <li><a class="dropdown-item" href="/org-users">Электронное портфолио</a></li>
+                                    <li><a class="dropdown-item" href="/organization-reports">Отчеты</a></li>
+                                    <li><a class="dropdown-item" href="/organization-documents">Документация</a></li>
+                                    <li><a class="dropdown-item" href="/dashboard/users/logout">Выйти</a></li>
+                                    <li class="dropdown-item organization-info"></li>
+                                </ul>
+                            </div>
+                        @else
+                            <a href="/login" class="nav-link">
+                            <span class="badge br-40 br-green-1 d-flex align-items-center"
+                                  style="padding-top: 7px; padding-bottom: 7px;">
+                                <span class="fs-16 text-black ps-1 pe-1">Вход</span>
+                            </span>
+                            </a>
+                        @endif
                     </li>
 
                 </ul>
