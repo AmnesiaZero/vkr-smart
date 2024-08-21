bf37c552a13ed65a5aa16f6e2acd30608aa2a3a5
Aleksandr Pavlov <sanek_pavlov03@mail.ru>
2024-08-21 16:19:46 +0400

    fix works display bug

--- app/Http/Controllers/Works/WorksController.php
+++ app/Http/Controllers/Works/WorksController.php
@@ -17,7 +17,7 @@ class WorksController extends Controller
 
     protected array $fillable = [
         'user_id',
-       'year_id',
+        'year_id',
         'faculty_id',
         'department_id',
         'specialty_id',
@@ -72,18 +72,16 @@ class WorksController extends Controller
     }
 
 
-
     public function get(Request $request): JsonResponse
     {
-        $validator = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             'page' => 'required|integer',
             'user_type' => 'required|integer|in:1,2',
-            'selected_departments.*' => ['integer',Rule::exists('departments','id')],
-            'user_id' => ['integer',Rule::exists('users','id')],
-            'selected_specialties.*' => ['integer',Rule::exists('programs_specialties','id')],
+            'selected_departments.*' => ['integer', Rule::exists('departments', 'id')],
+            'user_id' => ['integer', Rule::exists('users', 'id')],
+            'selected_specialties.*' => ['integer', Rule::exists('programs_specialties', 'id')],
         ]);
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return ValidatorHelper::error($validator);
         }
         $data = $request->only($this->fillable);
@@ -92,27 +90,26 @@ class WorksController extends Controller
 
     public function getUserWorks(Request $request)
     {
-        $validator = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             'page' => 'required|integer',
-            'user_id' =>  ['required','integer',Rule::exists('users','id')]
+            'user_id' => ['required', 'integer', Rule::exists('users', 'id')]
         ]);
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return ValidatorHelper::error($validator);
         }
         $pageNumber = $request->page;
         $userId = $request->user_id;
-        return $this->worksService->getUserWorks($userId,$pageNumber);
+        return $this->worksService->getUserWorks($userId, $pageNumber);
     }
 
-    public function create(Request $request):JsonResponse
+    public function create(Request $request): JsonResponse
     {
-        $validator = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             'user_type' => 'required|integer|in:1,2',
-            'year_id' => ['integer','required',Rule::exists('organizations_years','id')],
-            'faculty_id' => ['integer','required',Rule::exists('faculties','id')],
-            'department_id' => ['integer',Rule::exists('departments','id')],
-            'specialty_id' => ['integer',Rule::exists('programs_specialties','id')],
+            'year_id' => ['integer', 'required', Rule::exists('organizations_years', 'id')],
+            'faculty_id' => ['integer', 'required', Rule::exists('faculties', 'id')],
+            'department_id' => ['integer', Rule::exists('departments', 'id')],
+            'specialty_id' => ['integer', Rule::exists('programs_specialties', 'id')],
             'student' => 'max:250',
             'group' => 'required|max:250',
             'verification_method' => 'required|integer|in:0,1,2',
@@ -125,8 +122,7 @@ class WorksController extends Controller
             'self_check' => 'integer:in:1',
             'certificate_file' => 'file'
         ]);
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return ValidatorHelper::error($validator);
         }
         $data = $request->only($this->fillable);
@@ -135,16 +131,15 @@ class WorksController extends Controller
 
     public function import(Request $request): JsonResponse
     {
-        $validator = Validator::make($request->all(),[
-            'year_id' => ['integer','required',Rule::exists('organizations_years','id')],
-            'faculty_id' => ['integer','required',Rule::exists('faculties','id')],
-            'department_id' => ['integer','required',Rule::exists('departments','id')],
-            'specialty_id' => ['integer','required',Rule::exists('programs_specialties','id')],
+        $validator = Validator::make($request->all(), [
+            'year_id' => ['integer', 'required', Rule::exists('organizations_years', 'id')],
+            'faculty_id' => ['integer', 'required', Rule::exists('faculties', 'id')],
+            'department_id' => ['integer', 'required', Rule::exists('departments', 'id')],
+            'specialty_id' => ['integer', 'required', Rule::exists('programs_specialties', 'id')],
             'verification_method' => 'required|integer|in:0,1,2',
             'import_file' => 'required|file'
         ]);
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return ValidatorHelper::error($validator);
         }
         $data = $request->only($this->fillable);
@@ -153,9 +148,9 @@ class WorksController extends Controller
 
     public function search(Request $request): JsonResponse
     {
-        $validator = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             'user_type' => 'integer|in:1,2',
-            'specialty_id' => ['integer',Rule::exists('programs_specialties','id')],
+            'specialty_id' => ['integer', Rule::exists('programs_specialties', 'id')],
             'delete_type' => 'integer|in:0,1,2',
             'student' => 'max:250',
             'group' => 'max:250',
@@ -165,38 +160,35 @@ class WorksController extends Controller
             'name' => 'max:250',
             'selected_faculties.*' => ['integer', Rule::exists('faculties', 'id')],
             'selected_years.*' => ['integer', Rule::exists('organizations_years', 'id')],
-            'selected_departments.*' => ['integer',Rule::exists('departments','id')],
-            'selected_specialties.*' => ['integer',Rule::exists('programs_specialties','id')],
+            'selected_departments.*' => ['integer', Rule::exists('departments', 'id')],
+            'selected_specialties.*' => ['integer', Rule::exists('programs_specialties', 'id')],
             'user_id' => ['integer', Rule::exists('users', 'id')]
         ]);
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return ValidatorHelper::error($validator);
         }
         $data = $request->only($this->fillable);
         return $this->worksService->search($data);
     }
 
-    public function delete(Request $request):JsonResponse
+    public function delete(Request $request): JsonResponse
     {
-        $validator = Validator::make($request->all(),[
-            'id' => ['required','integer'],
+        $validator = Validator::make($request->all(), [
+            'id' => ['required', 'integer'],
         ]);
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return ValidatorHelper::error($validator);
         }
         $id = $request->id;
         return $this->worksService->delete($id);
     }
 
-    public function destroy(Request $request):JsonResponse
+    public function destroy(Request $request): JsonResponse
     {
-        $validator = Validator::make($request->all(),[
-            'id' => ['required','integer'],
+        $validator = Validator::make($request->all(), [
+            'id' => ['required', 'integer'],
         ]);
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return ValidatorHelper::error($validator);
         }
         $id = $request->id;
@@ -205,9 +197,9 @@ class WorksController extends Controller
 
     public function update(Request $request): JsonResponse
     {
-        $validator = Validator::make($request->all(),[
-            'id' => ['required','integer'],
-            'specialty_id' => ['integer',Rule::exists('programs_specialties','id')],
+        $validator = Validator::make($request->all(), [
+            'id' => ['required', 'integer'],
+            'specialty_id' => ['integer', Rule::exists('programs_specialties', 'id')],
             'student' => 'max:250',
             'group' => 'max:250',
             'scientific_supervisor' => 'max:250',
@@ -220,23 +212,21 @@ class WorksController extends Controller
             'certificate_file' => 'file',
             'work_status' => 'integer|in:0,1,2'
         ]);
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return ValidatorHelper::error($validator);
         }
         $id = $request->id;
-        Log::debug('id = '.$id);
+        Log::debug('id = ' . $id);
         $data = $request->only($this->fillable);
-        return $this->worksService->update($id,$data);
+        return $this->worksService->update($id, $data);
     }
 
     public function find(Request $request)
     {
-        $validator = Validator::make($request->all(),[
-            'id' => ['required','integer'],
+        $validator = Validator::make($request->all(), [
+            'id' => ['required', 'integer'],
         ]);
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return ValidatorHelper::error($validator);
         }
         $id = $request->id;
@@ -245,11 +235,10 @@ class WorksController extends Controller
 
     public function download(Request $request)
     {
-        $validator = Validator::make($request->all(),[
-            'id' => ['required','integer',Rule::exists('works','id')],
+        $validator = Validator::make($request->all(), [
+            'id' => ['required', 'integer', Rule::exists('works', 'id')],
         ]);
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return ValidatorHelper::error($validator);
         }
         $id = $request->id;
@@ -258,11 +247,10 @@ class WorksController extends Controller
 
     public function downloadCertificate(Request $request)
     {
-        $validator = Validator::make($request->all(),[
-            'id' => ['required','integer',Rule::exists('works','id')],
+        $validator = Validator::make($request->all(), [
+            'id' => ['required', 'integer', Rule::exists('works', 'id')],
         ]);
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return ValidatorHelper::error($validator);
         }
         $id = $request->id;
@@ -271,41 +259,38 @@ class WorksController extends Controller
 
     public function upload(Request $request): JsonResponse
     {
-        $validator = Validator::make($request->all(),[
-            'id' => ['required','integer'],
+        $validator = Validator::make($request->all(), [
+            'id' => ['required', 'integer'],
             'work_file' => 'required|file'
         ]);
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return ValidatorHelper::error($validator);
         }
         $id = $request->id;
         $workFile = $request->work_file;
-        return $this->worksService->upload($id,$workFile);
+        return $this->worksService->upload($id, $workFile);
     }
 
     public function uploadCertificate(Request $request): JsonResponse
     {
-        $validator = Validator::make($request->all(),[
-            'id' => ['required','integer'],
+        $validator = Validator::make($request->all(), [
+            'id' => ['required', 'integer'],
             'certificate_file' => 'required|file'
         ]);
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return ValidatorHelper::error($validator);
         }
         $id = $request->id;
         $certificate = $request->file('certificate_file');
-        return $this->worksService->uploadCertificate($id,$certificate);
+        return $this->worksService->uploadCertificate($id, $certificate);
     }
 
     public function copy(Request $request): JsonResponse
     {
-        $validator = Validator::make($request->all(),[
-            'id' => ['required','integer']
+        $validator = Validator::make($request->all(), [
+            'id' => ['required', 'integer']
         ]);
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return ValidatorHelper::error($validator);
         }
         $id = $request->id;
@@ -314,11 +299,10 @@ class WorksController extends Controller
 
     public function updateSelfCheckStatus(Request $request): JsonResponse
     {
-        $validator = Validator::make($request->all(),[
-            'id' => ['required','integer']
+        $validator = Validator::make($request->all(), [
+            'id' => ['required', 'integer']
         ]);
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return ValidatorHelper::error($validator);
         }
         $id = $request->id;
@@ -327,11 +311,10 @@ class WorksController extends Controller
 
     public function updateVisibility(Request $request): JsonResponse
     {
-        $validator = Validator::make($request->all(),[
-            'id' => ['required','integer']
+        $validator = Validator::make($request->all(), [
+            'id' => ['required', 'integer']
         ]);
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return ValidatorHelper::error($validator);
         }
         $id = $request->id;
@@ -340,11 +323,10 @@ class WorksController extends Controller
 
     public function restore(Request $request): JsonResponse
     {
-        $validator = Validator::make($request->all(),[
-            'id' => ['required','integer']
+        $validator = Validator::make($request->all(), [
+            'id' => ['required', 'integer']
         ]);
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return ValidatorHelper::error($validator);
         }
         $id = $request->id;
@@ -353,8 +335,8 @@ class WorksController extends Controller
 
     public function export(Request $request)
     {
-        $validator = Validator::make($request->all(),[
-            'specialty_id' => ['integer',Rule::exists('programs_specialties','id')],
+        $validator = Validator::make($request->all(), [
+            'specialty_id' => ['integer', Rule::exists('programs_specialties', 'id')],
             'delete_type' => 'integer|in:0,1,2',
             'student' => 'max:250',
             'group' => 'max:250',
@@ -364,11 +346,10 @@ class WorksController extends Controller
             'name' => 'max:250',
             'selected_faculties.*' => ['integer', Rule::exists('faculties', 'id')],
             'selected_years.*' => ['integer', Rule::exists('organizations_years', 'id')],
-            'selected_departments.*' => ['integer',Rule::exists('departments','id')],
-            'selected_specialties.*' => ['integer',Rule::exists('programs_specialties','id')],
+            'selected_departments.*' => ['integer', Rule::exists('departments', 'id')],
+            'selected_specialties.*' => ['integer', Rule::exists('programs_specialties', 'id')],
         ]);
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return ValidatorHelper::redirectError($validator);
         }
         $data = $request->only($this->fillable);
@@ -379,15 +360,26 @@ class WorksController extends Controller
     public function getReport(Request $request): JsonResponse
     {
         Log::debug('Вошёл в getReport');
-        $validator = Validator::make($request->all(),[
-            'document_id' => 'required|integer'
+        $validator = Validator::make($request->all(), [
+            'id' => 'required|integer'
         ]);
-        if($validator->fails())
-        {
+        if ($validator->fails()) {
             return ValidatorHelper::error($validator);
         }
         $documentId = $request->document_id;
         return $this->worksService->getReport($documentId);
+    }
+
+    public function showReport(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'id' => ['required','integer',Rule::exists('works','id')]
+        ]);
+        if ($validator->fails()) {
+            return ValidatorHelper::error($validator);
+        }
+        $id = $request->id;
+        return $this->worksService->showReport($id);
     }
 
 
