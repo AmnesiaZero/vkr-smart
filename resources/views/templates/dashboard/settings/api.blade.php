617977f651c8487bfe7422ca97875d5acfa6002d
Erkek <baxaisov.erke@yandex.ru>
2024-09-04 14:48:37 +0400

    fixed settings api layout

--- resources/views/templates/dashboard/settings/api.blade.php
+++ resources/views/templates/dashboard/settings/api.blade.php
@@ -3,35 +3,10 @@
 @section('content')
     <div class="col-xl-9 col-lg-8 col-md-7 col-12">
         <div class="row px-5 pt-5 g-3">
-            <div class="col-xxl-6 col-lg-10 col-12">
-                <div class="br-green-light-2 br-15 p-3 mb-3">
-                    <div class="row g-4">
-                        <div class="col">
-                            <img src="/images/VKR-logo.svg" alt="">
-                        </div>
-                        <div class="col d-flex flex-column justify-content-between">
-                            <p class="fs-16 m-0">
-                                <img src="/images/href.svg" alt="" class="pe-2">
-                                <a href="#" class="text-grey td-none text-grey-hover fw-600">
-                                    ВКР-ВУЗ API
-                                </a>
-                            </p>
-                            <div class="mt-3">
-                                <p class="fs-16 m-0">
-                                    <img src="/images/href.svg" alt="" class="pe-2">
-                                    <a href="#" class="text-grey td-none text-grey-hover fw-600">
-                                        Документация к сервису
-                                    </a>
-                                </p>
-                                <p class="text-grey fs-14 mt-2 mb-0">Основной перечень запросов ВКР-ВУЗ API</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
         </div>
         <div class="row">
-            <form onsubmit="generateApiKey();return false" id="generate_key_form">
+            <form class="row" onsubmit="generateApiKey();return false" id="generate_key_form">
                 <div class="col-xxl-5 col-xl-6 col-lg-7 mb-lg-0 mb-md-5 mb-0 p-5 col-12">
                     <p class="fw-600">«Подключение и настройка»</p>
                     <p class="fs-14 text-grey lh-17">Для подключения к сервису Вам необходимы 3 параметра:<br>2
@@ -62,6 +37,32 @@
                         Создать ключ защиты API
                     </button>
                 </div>
+                <div id="vkr-logo" class="col-xxl-6 col-lg-10 col-12">
+                    <div class="br-green-light-2 br-15 p-3 mb-3">
+                        <div class="row g-4">
+                            <div class="col">
+                                <img src="/images/VKR-logo.svg" alt="">
+                            </div>
+                            <div class="col d-flex flex-column justify-content-between">
+                                <p class="fs-16 m-0">
+                                    <img src="/images/href.svg" alt="" class="pe-2">
+                                    <a href="#" class="text-grey td-none text-grey-hover fw-600">
+                                        ВКР-ВУЗ API
+                                    </a>
+                                </p>
+                                <div class="mt-3">
+                                    <p class="fs-16 m-0">
+                                        <img src="/images/href.svg" alt="" class="pe-2">
+                                        <a href="#" class="text-grey td-none text-grey-hover fw-600">
+                                            Документация к сервису
+                                        </a>
+                                    </p>
+                                    <p class="text-grey fs-14 mt-2 mb-0">Основной перечень запросов ВКР-ВУЗ API</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </form>
         </div>
     </div>
@@ -72,8 +73,9 @@
     <script src="/js/dashboard/settings/api.js"></script>
 
     <script type="text/x-jquery-tmpl" id="jwt_tmpl">
-      <span> Ваш jwt ключ - ${token} </span>
-
-
+        <span class="fs-20">
+            Ваш JWT-ключ: <br>
+            <span class="fs-24 fw-bold text-green">${token}</span>
+        </span>
     </script>
 @endsection
