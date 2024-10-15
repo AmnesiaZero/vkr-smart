94ed68b217918cd32fcb8a28536f436f67dd95b0
Aleksandr Pavlov <sanek_pavlov03@mail.ru>
2024-10-15 14:08:43 +0400

    fix check code generation

--- app/Services/Works/WorksService.php
+++ app/Services/Works/WorksService.php
@@ -435,7 +435,7 @@ class WorksService extends Services
         Log::debug('unique = ' . $unique);
         $work = $this->workRepository->findByReportId($documentId);
         $workId = $work->id;
-        $checkCode = random_bytes(6);
+        $checkCode = rand();
         $data = [
             'report_status' => 1,
             'unique_percent' => $unique,
