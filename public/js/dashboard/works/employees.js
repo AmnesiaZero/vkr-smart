b89bc485775282bfcbcd5281a5a5f18e0ecf0928
Erkek <baxaisov.erke@yandex.ru>
2024-10-24 15:44:43 +0400

    add functional to full report on the work

--- public/js/dashboard/works/employees.js
+++ public/js/dashboard/works/employees.js
@@ -830,6 +830,9 @@ function openReport(workId)
                 const work = response.data.work;
                 $("#report_container").html($("#report_tmpl").tmpl(work));
                 const modalElement = new bootstrap.Modal(document.getElementById('report_modal'));
+
+
+
                 modalElement.show();
             }
             else
@@ -1488,4 +1491,137 @@ $(document).ready(function() {
             $('#scientific_supervisor_input').prop('disabled', false);
         }
     });
+});
+
+
+let isFirstOpen = true;
+
+$('#report_modal').on('shown.bs.modal', function () {
+    if (isFirstOpen) {
+        $('#report_modal .modal-body').html(`
+            <div id="report_content">
+                <p> Полный отчет по работе </p>
+                <div class="d-flex justify-content-between">
+                    <a ng-if="report.status_report !== '1'" id="print_report" class="btn btn-success">
+                        <span class="glyphicon glyphicon-print"></span>
+                        Распечатать отчет
+                    </a>
+                    <a ng-if="report.status_report !== '1'" id="vkr_reference" class="btn btn-warning">
+                        <span class="glyphicon glyphicon-download"></span>
+                        Справка ВКР
+                    </a>
+                </div>
+                <div class="col-sm-8 mt-2">
+                    <ol style="padding-left:15px;">
+                        <li>Результаты проверки по базам данных ВКР-СМАРТ:
+                            <ul>
+                                <li>Оригинальность текста документа: <strong id="borrowings_percent">${unique_percent}%</strong></li>
+                            </ul>
+                            <ul>
+                                <li>Код справки: <strong id="check_code">${id}-${check_code}</strong></li>
+                            </ul>
+                        </li>
+                    </ol>
+                </div>
+                <table class="table table-mini table-bordered table-condensed mt-4">
+                    <thead>
+                        <tr>
+                            <th>Источник</th>
+                            <th>Ссылка на источник</th>
+                            <th>Коллекция/модуль поиска</th>
+                            <th>Доля в отчете</th>
+                        </tr>
+                    </thead>
+                    <tbody id="report_assets_list">
+                        <!-- Пример данных из шаблона -->
+                    </tbody>
+                </table>
+            </div>
+        `);
+        isFirstOpen = false;
+    }
+});
+
+$(document).on('click', '#vkr_reference', function() {
+    $('#report_modal .modal-body').html(`
+        <div ng-view="" class="view-animate ng-scope" autoscroll="">
+            <div class="container certificate-container">
+                <div class="row">
+                    <a href="#" onclick="window.print(); return false;" class="btn btn-link btn-smart">
+                        <span class="glyphicon glyphicon-print"></span> Распечатать справку
+                    </a>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12 text-center">
+                        <h2>СПРАВКА</h2>
+                        <h3>о результатах проверки на наличие заимствований</h3>
+                        <p style="color: #006f92;font-size: 16px;padding-top: 10px;">
+                            Уникальный код справки: <strong class="ng-binding">1-243735-35349</strong>
+                        </p>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-12 text-bolder">Ф.И.О. автора проверяемой работы: Тестовая работа</div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-12 text-bolder">Тема работы: 33</div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-12 text-bolder">Руководитель: Кузьмичев Николай Валерьевич</div>
+                </div>
+                <hr>
+                <h3 class="text-center">Источники цитирования *</h3>
+                <table class="table table-bordered table-condensed">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Доля в отчете</th>
+                            <th>Источник (ссылка)</th>
+                            <th>Где найдено (Модуль поиска)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>48.36%</td>
+                            <td>Работа студента «Пугачев Сергей Александрович»</td>
+                            <td>Модуль поиска Интернет</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>2.02%</td>
+                            <td>Ядерное оружие. Организация спасательных работ</td>
+                            <td>Модуль поиска Интернет</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="row">
+                    <div class="col-sm-12 text-center" style="font-size:28px">Уникальность текста:  48.16%</div>
+                </div>
+                <table class="table table-mini">
+                    <tbody>
+                        <tr>
+                            <td><hr></td>
+                            <td><hr></td>
+                            <td><hr></td>
+                            <td><hr></td>
+                        </tr>
+                        <tr>
+                            <td class="">подпись студента</td>
+                            <td>расшифровка подписи</td>
+                            <td>подпись ответственного за проверку</td>
+                            <td>расшифровка подписи</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    `);
+});
+
+$(document).on('click', '#print_report', function() {
+    window.print();  // Вызов функции печати браузера
 });
--- resources/views/layouts/dashboard/include/tmpls/works/report.blade.php
+++ resources/views/layouts/dashboard/include/tmpls/works/report.blade.php
@@ -1,66 +1,102 @@
-<script id="report_tmpl" type="text/x-jquery-tmpl">
-    <div class="modal fade"  id="report_modal">
-       <div class="modal-dialog modal-lg modal-dialog-centered">
-           <div class="modal-content">
-               <div class="modal-header">
-                   <h3 class="modal-title">Полный отчет по работе</h3>
-               </div>
-               <div class="modal-body">
-                   <div id="report_modal">
-                       <span> Полный отчет по работе </span>
+<div class="modal fade" id="report_modal">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title">Полный отчет по работе</h3>
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <!-- Основное содержимое будет динамически подставляться через JS -->
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть окно</button>
+            </div>
+        </div>
+    </div>
+</div>
 
-                       <a ng-if="report.status_report !== '1'" ng-click="showFull()" class="btn btn-success ng-scope"><span class="glyphicon glyphicon-print"></span> Распечатать отчет</a>
-                       <a ng-if="report.status_report !== '1'" href="#/reference/530250/1/cd9640cd3f886e13072731fc90e66715" class="btn btn-warning ng-scope"><span class="glyphicon glyphicon-download"></span> Справка ВКР</a>
 
-                       <div class="col-sm-8 mt-2">
-                           <ol style="padding-left:15px;">
-                               <li>Результаты проверки по базам данных ВКР-СМАРТ:
-                                   <ul>
-                                       <li>Оригинальность текста документа: <strong id="borrowings_percent" class="ng-binding">${unique_percent}%</strong></li>
-                                   </ul>
-                                   <ul>
-                                       <li>Код справки: <strong id="check_code" class="ng-binding">${id}-${check_code}</strong></li>
-                                   </ul>
-                               </li>
-                           </ol>
-                       </div>
-                       <table class="table table-mini table-bordered table-condensed ng-scope mt-4">
-                           <thead>
-                           <tr>
-                               <th>Источник</th>
-                               <th>Ссылка на источник</th>
-                               <th>Коллекция/модуль поиска</th>
-                               <th>Доля в отчете</th>
-                           </tr>
-                           </thead>
-                           <tbody id="report_assets_list">
-                               @{{each report_assets}}
-                               <tr>
-                                   <td>
-                                       <a target="_blank" class="ng-binding fs-14" href="${link}">${name}</a>
-                                   </td>
-                                   <td>
-                                       <a target="_blank" href="${link}" class="ng-binding">${link}</a>
-                                   </td>
-                                   <td>
-                                       Интернет
-                                   </td>
-                                   <td class="ng-binding">
-                                       ${borrowings_percent}%
-                                   </td>
-                               </tr>
-                               @{{/each}}
-                           </tbody>
-                       </table>
-                   </div>
-               </div>
-               <div class="modal-footer">
-                   <button type="button" class="btn btn-grey border-radius-5 fs-14 text-grey py-1" data-bs-dismiss="modal" aria-label="Close">
-                       Закрыть окно
-                   </button>
-               </div>
-           </div>
-       </div>
-   </div>
 
-</script>
+
+
+
+
+
+
+
+
+{{--<script id="report_tmpl" type="text/x-jquery-tmpl">--}}
+{{--    <div class="modal fade"  id="report_modal">--}}
+{{--       <div class="modal-dialog modal-lg modal-dialog-centered">--}}
+{{--           <div class="modal-content">--}}
+{{--               <div class="modal-header">--}}
+{{--                   <h3 class="modal-title">Полный отчет по работе</h3>--}}
+{{--               </div>--}}
+{{--               <div class="modal-body">--}}
+{{--                   <div id="report_modal">--}}
+{{--                       <p> Полный отчет по работе </p>--}}
+
+{{--                       <div class="d-flex justify-content-between">--}}
+{{--                           <a ng-if="report.status_report !== '1'" ng-click="showFull()" class="btn btn-success ng-scope">--}}
+{{--                               <span class="glyphicon glyphicon-print"></span>--}}
+{{--                               Распечатать отчет--}}
+{{--                           </a>--}}
+{{--                           <a ng-if="report.status_report !== '1'" href="#/reference/530250/1/cd9640cd3f886e13072731fc90e66715" class="btn btn-warning ng-scope">--}}
+{{--                               <span class="glyphicon glyphicon-download"></span>--}}
+{{--                               Справка ВКР--}}
+{{--                           </a>--}}
+{{--                       </div>--}}
+
+{{--                       <div class="col-sm-8 mt-2">--}}
+{{--                           <ol style="padding-left:15px;">--}}
+{{--                               <li>Результаты проверки по базам данных ВКР-СМАРТ:--}}
+{{--                                   <ul>--}}
+{{--                                       <li>Оригинальность текста документа: <strong id="borrowings_percent" class="ng-binding">${unique_percent}%</strong></li>--}}
+{{--                                   </ul>--}}
+{{--                                   <ul>--}}
+{{--                                       <li>Код справки: <strong id="check_code" class="ng-binding">${id}-${check_code}</strong></li>--}}
+{{--                                   </ul>--}}
+{{--                               </li>--}}
+{{--                           </ol>--}}
+{{--                       </div>--}}
+{{--                       <table class="table table-mini table-bordered table-condensed ng-scope mt-4">--}}
+{{--                           <thead>--}}
+{{--                           <tr>--}}
+{{--                               <th>Источник</th>--}}
+{{--                               <th>Ссылка на источник</th>--}}
+{{--                               <th>Коллекция/модуль поиска</th>--}}
+{{--                               <th>Доля в отчете</th>--}}
+{{--                           </tr>--}}
+{{--                           </thead>--}}
+{{--                           <tbody id="report_assets_list">--}}
+{{--                               @{{each report_assets}}--}}
+{{--                               <tr>--}}
+{{--                                   <td>--}}
+{{--                                       <a target="_blank" class="ng-binding fs-14" href="${link}">${name}</a>--}}
+{{--                                   </td>--}}
+{{--                                   <td>--}}
+{{--                                       <a target="_blank" href="${link}" class="ng-binding">${link}</a>--}}
+{{--                                   </td>--}}
+{{--                                   <td>--}}
+{{--                                       Интернет--}}
+{{--                                   </td>--}}
+{{--                                   <td class="ng-binding">--}}
+{{--                                       ${borrowings_percent}%--}}
+{{--                                   </td>--}}
+{{--                               </tr>--}}
+{{--                               @{{/each}}--}}
+{{--                           </tbody>--}}
+{{--                       </table>--}}
+{{--                   </div>--}}
+{{--               </div>--}}
+{{--               <div class="modal-footer">--}}
+{{--                   <button type="button" class="btn btn-grey border-radius-5 fs-14 text-grey py-1" data-bs-dismiss="modal" aria-label="Close">--}}
+{{--                       Закрыть окно--}}
+{{--                   </button>--}}
+{{--               </div>--}}
+{{--           </div>--}}
+{{--       </div>--}}
+{{--   </div>--}}
+{{--</script>--}}
