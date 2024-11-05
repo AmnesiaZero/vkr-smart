977fb7cca33f67e7a3c651f25d6ac7b725d601e7
Aleksandr Pavlov <sanek_pavlov03@mail.ru>
2024-11-05 10:03:52 +0400

    add delete for work types and scientific supervisors + icon (it somehow was lost in some update) + command to generate app key

--- resources/views/templates/dashboard/settings/handbook_management.blade.php
+++ resources/views/templates/dashboard/settings/handbook_management.blade.php
@@ -50,6 +50,7 @@
                             <div class="badge text-black-black bg-green-light br-100 fs-14 me-1 mb-1"
                                  id="work_type_{{$works_type->id}}">
                                 {{$works_type->name}}
+                                <i class="fa fa-times ms-2 cursor-p text-black fs-12" onclick="deleteWorkType({{$works_type->id}})"></i>
                             </div>
                         @endforeach
                     @endif
@@ -68,6 +69,7 @@
                             <div class="badge text-black-black bg-green-light br-100 fs-14 me-1 mb-1"
                                  id="scientific_supervisor_{{$scientific_supervisor->id}}">
                                 {{$scientific_supervisor->name}}
+                                <i class="fa fa-times ms-2 cursor-p text-black fs-12" onclick="deleteScientificSupervisor({{$scientific_supervisor->id}})"></i>
                             </div>
                         @endforeach
                     @endif
@@ -83,10 +85,16 @@
     </script>
 
     <script id="scientific_supervisor_tmpl" type="text/x-jquery-tmpl">
-       <div class="badge text-black-black bg-green-light br-100 fs-14 me-1 mb-1" onclick="deleteScientificSupervisor(${id})" id="scientific_supervisor_${id}">${name}</div>
+       <div class="badge text-black-black bg-green-light br-100 fs-14 me-1 mb-1" onclick="deleteScientificSupervisor(${id})" id="scientific_supervisor_${id}">
+       ${name}
+         <i class="fa fa-times ms-2 cursor-p text-black fs-12" onclick="deleteWorkType(${id})"></i>
+       </div>
     </script>
 
     <script id="works_type_tmpl" type="text/x-jquery-tmpl">
-       <div class="badge text-black-black bg-green-light br-100 fs-14 me-1 mb-1" onclick="deleteWorkType(${id})" id="work_type_${id}">${name}</div>
+       <div class="badge text-black-black bg-green-light br-100 fs-14 me-1 mb-1" onclick="deleteWorkType(${id})" id="work_type_${id}">
+       ${name}
+        <i class="fa fa-times ms-2 cursor-p text-black fs-12" onclick="deleteWorkType(${id})"></i>
+       </div>
     </script>
 @endsection
